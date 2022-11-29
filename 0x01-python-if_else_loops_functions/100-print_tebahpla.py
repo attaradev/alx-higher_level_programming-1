a98ef3600ceabbd3b93f2843e80036d31f98a013
Ashdof <mzigah4@gmail.com>
2022-11-29 20:55:13 +0000

    Task 14: fixed an error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-i = 0
-for ch in range(ord('z'), ord('a') - 1, -1):
-    print('{}"'.format(chr(ch - i)), end='')
-    i = 32 if i == 0 else 0
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i -= 32
+    print("{}".format(chr(i)), end='')
