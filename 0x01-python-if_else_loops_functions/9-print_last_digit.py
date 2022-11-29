8b25a6f89b4891e92b4b34c503c72ae8924380e1
Ashdof <mzigah4@gmail.com>
2022-11-29 16:46:40 +0000

    Task 9: a script to print the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if not isinstance(number, int):
+        print('ValueError')
+
+    last_digit = int(repr(number)[-1])
+    if number < 0:
+        last_digit *= -1
+
+    print('{:d}'.format(last_digit))
+    return last_digit
