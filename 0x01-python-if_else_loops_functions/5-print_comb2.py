db9161651eb508c69622f4ba82dbe753a877d5dc
Ashdof <mzigah4@gmail.com>
2022-11-29 15:17:15 +0000

    Task 5: a script that prints numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print('{}'.format('0' + str(i)), end=', ')
-    elif i == 99:
+    if i == 99:
         print(i)
     else:
-        print('{}'.format(i), end=', ')
+        print('{}'.format('0' + str(i) if i < 10 else i), end=', ')
