0b20d5190723ad0f06b42fe234e02de5af6dfe06
Ashdof <mzigah4@gmail.com>
2022-11-29 20:40:47 +0000

    Task 1: fixed a typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(repr(number)[-1])
+lastDigit = 0
 
-if number < 0:
-    last_digit *= -1
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = (-number % 10) * -1
 
-msg = f'Last digit of {number} is {last_digit}'
+message = f"Last digit of {number} is {lastDigit}"
 
-if last_digit > 5:
-    print(f'{msg} and is greater than 5')
-elif last_digit == 0:
-    print(f'{msg} and is 0')
-elif last_digit < 6 and not 0:
-    print(f'{msg} and is less than 6 and not 0')
+if lastDigit == 0:
+    print(f"{message} and is 0")
+elif lastDigit > 5 and lastDigit % 10 != 0:
+    print(f"{message} and is greater than 5")
+else:
+    print(f"{message} and is less than 6 and not 0")
